41f2aaf09947f2e2850b1f462d4792f0948f0e10
victor <luanvitorlima2017@gmail.com>
2026-05-25 18:03:07 -0300

    fix: nova nota e remocao de ids

--- src/pages/Notas.jsx
+++ src/pages/Notas.jsx
@@ -3,54 +3,48 @@ import grogu from "../assets/images/notas/grogu.jpg";
 
 export const notes = [
   {
-    id: 10,
     data: "24 de maio 2026",
     text: "imprimir cortadores com temática de halloween e fiz alguns biscoitos, ficaram mt bom (preciso lembrar de subir as fotos aqui)"
   },
   {
-    id: 1,
+    date: "23 de maio 2026",
+    text: "fix a pintura da espada da minha namorada e de alguns outros modelos em 3D que eu havia feito"
+  },
+  {
     date: "16 maio 2026",
     text: "fiz um charmander e um Grogu (Bebe Yoda), o charmander vai ser pintado amanhã, meu irmão pintou o grogu hj",
     image: grogu,
   },
   {
-    id: 2,
     date: "16 maio 2026",
     text: "entreguei uma das encomendas em 3D para uma cliente, canetinhas acrilicas chegaram e os projetos estão mais bonitinhos",
   },
   {
-    id: 3,
     date: "15 maio 2026",
     text: "comprei um nintendo switch lite desbloqueado... foi o impulso, não me arrependo",
   },
   {
-    id: 4,
     date: "13 maio 2026",
     text: "Peguei meu notebook na assistencia técnica, ele estava com o teclado zoado.",
   },
   {
-    id: 5,
     date: "13 maio 2026",
     text: 'Desenvolvi o "SearchPeople (SP)" no trabalho, ele consegue encontrar dados de pessoas com cadastro ativo do Cadúnico.',
   },
   {
-    id: 6,
     date: "12 maio 2026",
     text: "Fiz o design no estilo marionete com Photoshop e After Effects.",
   },
   {
-    id: 7,
     date: "12 maio 2026",
     text: "comecei esse site inspirado em colagem e sites antigos",
   },
   {
-    id: 8,
     date: "11 maio 2026",
     text: "imprimi um marca paginas de gatinho usando a impressora 3D.",
     image: marcaPagina,
   },
   {
-    id: 9,
     date: "08 maio 2026",
     text: "organizei minhas texturas, projetos e imagens antigas.",
   },
