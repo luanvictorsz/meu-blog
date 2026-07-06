b213bdb0d3ae8a61fd2489e4a048be9840731b6d
victorsz <luanvitorlima2017@gmail.com>
2026-07-05 21:14:16 -0300

    feat: adicionando imagem do resident evil na lista de jogos

--- src/components/pages/Jogos.jsx
+++ src/components/pages/Jogos.jsx
@@ -56,6 +56,12 @@ const jogos = [
     platinado: false },
     
   // ── OUTROS JOGOS NÃO PLATINADOS
+  
+  {
+    titulo: "Resident evil",
+    imagem: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/4249100/c6a3c50a9fd16bb11cf5d1a3f551fa7edcab09ef/capsule_616x353.jpg",
+    platinado: false
+  },
   { titulo: 'Clair Obscur: Expedition 33', 
     imagem: 'https://cdn.akamai.steamstatic.com/steam/apps/1903340/header.jpg', 
     platinado: false },
